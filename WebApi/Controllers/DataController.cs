af82271662ac4298deddb7e40bb12a09074feabe
Denis Malimon <malimon88@gmail.com>
2020-10-07 13:37:30 +0300

    add data controller

---
+++ WebApi/Controllers/DataController.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        private readonly ICipher _cipher;
+
+        public DataController(ICipher cipher)
+        {
+            _cipher = cipher;
+        }
+
+        [HttpPost("encrypt")]
+        public IActionResult Encrypt([FromBody] string payload)
+        {
+            var result = _cipher.Encrypt(payload);
+            
+            return Ok(result);
+        }
+
+        [HttpPost("decrypt")]
+        public IActionResult Decrypt([FromBody] string payload)
+        {
+            var result = _cipher.Decrypt(payload);
+
+            return Ok(result);
+        }
+    }
+}
