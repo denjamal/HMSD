54c8ebefc4023b3777ebc85dbf0ae5a21b76c46f
Denis Malimon <malimon88@gmail.com>
2020-10-07 16:47:19 +0300

    implement data proxy controller

---
+++ WebApi.Gateway/Controllers/DataController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Gateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public DataController(IHttpClientFactory factory)
+        {
+            var client = factory.CreateClient();
+            client.BaseAddress = new Uri("http://localhost:50395/api/");
+            _httpClient = client;
+        }
+
+        [HttpPost("encrypt")]
+        public async Task<IActionResult> EncryptAsync([FromBody] string payload)
+        {
+            var requestData = JsonSerializer.Serialize(payload);
+            var response = await _httpClient.PostAsync("data/encrypt", new StringContent(requestData, Encoding.UTF8, "application/json"));
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+                return Ok(await response.Content.ReadAsStringAsync());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message);
+            }
+        }
+
+        [HttpPost("decrypt")]
+        public async Task<IActionResult> DecryptAsync([FromBody] string payload)
+        {
+            var requestData = JsonSerializer.Serialize(payload);
+            var response = await _httpClient.PostAsync("data/decrypt", new StringContent(requestData, Encoding.UTF8, "application/json"));
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+                return Ok(await response.Content.ReadAsStringAsync());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message);
+            }
+        }
+
+        [HttpPost("rotateKeys")]
+        public async Task<IActionResult> RotateAsync()
+        {
+            var response = await _httpClient.PostAsync("keys/rotate", null);
+
+            try
+            {
+                response.EnsureSuccessStatusCode();
+                return Ok(await response.Content.ReadAsStringAsync());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message);
+            }
+        }
+    }
+}
--- WebApi.Gateway/Startup.cs
+++ WebApi.Gateway/Startup.cs
@@ -19,6 +19,7 @@ namespace WebApi.Gateway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
