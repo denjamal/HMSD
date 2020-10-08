47a29dc7e48bf7022bf28bbd77c0658fc0b71c0c
Denis Malimon <malimon88@gmail.com>
2020-10-08 10:02:22 +0300

    add nswag

--- WebApi.Gateway/Startup.cs
+++ WebApi.Gateway/Startup.cs
@@ -30,11 +30,17 @@ namespace WebApi.Gateway
             });
             services.AddResponseCompression();
             services.AddHostedService<RotateKeysBackgroundService>();
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
